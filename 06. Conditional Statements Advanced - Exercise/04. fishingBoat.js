bcf48ce171b22fa9481b22306d698d9b95a8b952
NadezhdaGeorgieva <nadezhdageorgieva@outlook.com>
2021-02-23 19:31:59 +0200

    Javascript Basics Exercises

---
+++ 06. Conditional Statements Advanced - Exercise/04. fishingBoat.js
@@ -0,0 +1,36 @@
+function fishingBoat(input) {
+    let groupBudget = parseInt(input[0]);
+    let season = input[1];
+    let fishermen = parseInt(input[2]);
+  
+    let rent = {
+      Spring: 3000,
+      Summer: 4200,
+      Autumn: 4200,
+      Winter: 2600
+    }
+  
+    rent = rent[season]; // rent[key] returns value
+    
+    // Substract discount depending on number of fishermen
+    if (fishermen <= 6) {
+      rent -= (10 / 100) * rent;
+    } else if (fishermen > 6 && fishermen <= 11) {
+      rent -= (15 / 100) * rent;
+    } else if (fishermen > 11) {
+      rent -= (25 / 100) * rent;
+    }
+    // Substract discount if even number of fishermen
+    if (fishermen % 2 == 0 && season != "Autumn") {
+      rent -= (5 / 100) * rent;
+    }
+  
+    if (groupBudget >= rent) {
+      let diff = groupBudget - rent;
+      console.log(`Yes! You have ${diff.toFixed(2)} leva left.`);
+    } else {
+      let diff = Math.abs(rent - groupBudget);
+      console.log(`Not enough money! You need ${diff.toFixed(2)} leva.`);
+    }
+  
+}
